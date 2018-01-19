c2991a3bc360c50e2a2a6cd2711509a8569e1267
RQMplays <benoit.jacques.felix@gmail.com>
2018-01-19 10:25:06 +0100

    starting building Apprenant ObjectFactory

---
+++ main.js
@@ -0,0 +1,12 @@
+const arrayApprenant = [
+	{firstname:},
+];
+
+function Apprenant (firstname, lastname, photoUrl, cvUrl, portfolioUrl, desc ) {
+	this.firstname = firstname
+	this.lastname = lastname
+	this.photoUrl = photoUrl
+	this.cvUrl = cvUrl
+	this.portfolioUrl = porfolioUrl
+	this.desc = desc
+}
