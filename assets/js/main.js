c7fb2ea0334702c64f2bb7bb12dca831708e4d18
RQMplays <benoit.jacques.felix@gmail.com>
2018-01-22 11:43:49 +0100

    add Date animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -223,7 +223,9 @@ let mainElement = document.getElementById('main');
 let lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
 
 // Smooth scroll
-let scroll = new SmoothScroll('a[href*="#"]')
+let scroll = new SmoothScroll('a[href*="#"]', {
+	header: 'nav'
+});
 
 randomButton.onclick = function(){
 	populateTheDom(arrayApprenant)
